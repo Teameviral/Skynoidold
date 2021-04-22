b4ab728534cb6d23a1fd1cd3baf9985a2770b410
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2021-04-22 23:43:53 +0530

    Update stylish.py

--- skynoid/plugins/stylish.py
+++ skynoid/plugins/stylish.py
@@ -3,10 +3,10 @@ from html import escape
 
 from pyrogram import filters
 
-from nana import AdminSettings
-from nana import app
-from nana import COMMAND_PREFIXES
-from nana import edit_or_reply
+from skynoid import AdminSettings
+from skynoid import app
+from skynoid import COMMAND_PREFIXES
+from skynoid import edit_or_reply
 
 __MODULE__ = 'Stylish Text'
 __HELP__ = """
