52e81870b8e04138696a2112907e0ab31a7ea584
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2021-04-22 22:17:32 +0530

    Update mega_dl.py

--- skynoid/plugins/mega_dl.py
+++ skynoid/plugins/mega_dl.py
@@ -4,12 +4,11 @@ from glob import glob
 from mega import Mega
 from pyrogram import filters
 
-from nana import AdminSettings
-from nana import app
-from nana import COMMAND_PREFIXES
-from nana import edit_or_reply
+from skynoid import AdminSettings
+from skynoid import app
+from skynoid import edit_or_reply
 
-__MODULE__ = 'Mega'
+__MODULE__ = '▲ Mega ▼'
 __HELP__ = """
 Download any file from URL or Telegram.
 
@@ -26,7 +25,7 @@ Give url as args to download it.
 
 async def megadl(url):
     mega = Mega()
-    mega.download_url(url, 'nana/downloads/mega')
+    mega.download_url(url, 'skynoid/downloads/mega')
 
 
 @app.on_message(
@@ -38,10 +37,10 @@ async def mega_download(_, message):
         await edit_or_reply(message, text='Usage: mega (url)')
         return
     await edit_or_reply(message, text='__Processing...__')
-    if not os.path.exists('nana/downloads/mega'):
-        os.makedirs('nana/downloads/mega')
+    if not os.path.exists('skynoid/downloads/mega'):
+        os.makedirs('skynoid/downloads/mega')
     await megadl(args[1])
-    files_list = glob('nana/downloads/mega/*')
+    files_list = glob('skynoid/downloads/mega/*')
     for doc in files_list:
         await message.reply_document(doc)
         os.remove(doc)
